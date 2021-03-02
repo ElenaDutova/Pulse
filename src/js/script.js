0070f2931132259669d022f48c284e70499ac7e3
ElenaDutova <eluvarova0997@mail.ru>
2021-03-02 22:04:21 +0300

    Animation

--- src/js/script.js
+++ src/js/script.js
@@ -85,6 +85,10 @@ $(document).ready(function(){
 
     $('form').submit(function(e) {
         e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
